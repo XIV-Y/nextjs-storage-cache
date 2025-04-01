cb3465a257717ceb2704908b4f7594d59692ca9d
XIV-Y <yutodazepa.g@gmail.com>
2025-04-01 23:43:32 +0900

    Setting:  各種環境設定を行う

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://jsonplaceholder.typicode.com/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
